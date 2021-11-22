6763c12b2d9cb983c10975a96061f8705dc7e515
JMComstock <jasoncomstock11@gmail.com>
2021-11-22 13:26:46 -0500

    first exercse comitted

---
+++ primitiveTypes.js
@@ -0,0 +1,38 @@
+// Identify the data type of the following values:
+
+    99.9
+console.log(typeof 99.9);
+    "false"
+console.log(typeof "false");
+    false
+console.log(typeof false);
+    '0'
+console.log(typeof '0');
+    0
+console.log(typeof 0);
+    true
+console.log(typeof true);
+    'true'
+console.log(typeof 'true');
+/*
+What data type would best represent:
+
+    A term or phrase typed into a search box?
+    string
+
+    If a user is logged in?
+    boolean
+
+    A discount amount to apply to a user's shopping cart?
+    number
+
+    Whether or not a coupon code is valid?
+    boolean
+
+    An email address typed into a registration form?
+    string
+
+    The price of a product?
+    number
+
+*/
