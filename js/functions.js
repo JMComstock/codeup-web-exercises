9e3e1953ad12b577b3a2b4c8234118693190cf49
JMComstock <jasoncomstock11@gmail.com>
2021-11-23 17:14:48 -0500

    modified comment sections

--- js/functions.js
+++ js/functions.js
@@ -38,8 +38,10 @@ var myName = "Jason";
 console.log(sayHello(myName));
 
 
-Don't modify the following line, it generates a random number between 1 and 3
-and stores it in a variable named random
+/**
+ * Don't modify the following line, it generates a random number between 1 and 3
+ * and stores it in a variable named random
+ */
 var random = Math.floor((Math.random() * 3) + 1);
 
 /**
