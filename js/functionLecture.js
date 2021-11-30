a7ccdc863f557c1b53300d2d0b0d43e3f792f0cc
JMComstock <jasoncomstock11@gmail.com>
2021-11-30 11:05:54 -0600

    added more functions to functionLecture.js

--- js/functionLecture.js
+++ js/functionLecture.js
@@ -1,4 +1,4 @@
-//
+"use strict";
 // function tryToGetADrink(isAtBar, bac, age, cashOnHand) { // the function definition contains PARAMETERS
 //
 //     let canHaveADrink = (isAtBar && bac < .08 && age >= 21 && cashOnHand >= 5.00)
@@ -65,19 +65,99 @@
 
 // FUNCTION SCOPING
 
-"use strict";
+// function tellMeIfDoggoWasGood() {
+//     var isAGoodDog = isAGoodDog;
+//     return isAGoodDog
+//         ? "Such a good boy!"
+//         : "Eh, hes had better days";
+// }
+// function wereTheyReallyAGoodDog() {
+//     var isAGoodDog = true;
+//     return isAGoodDog;
+// }
+// console.log(wereTheyReallyAGoodDog());
+// console.log(tellMeIfDoggoWasGood());
 
-function tellMeIfDoggoWasGood() {
-    var isAGoodDog = isAGoodDog;
-    return isAGoodDog
-        ? "Such a good boy!"
-        : "Eh, hes had better days";
-}
-function wereTheyReallyAGoodDog() {
-    var isAGoodDog = true;
-    return isAGoodDog;
-}
-console.log(wereTheyReallyAGoodDog());
-console.log(tellMeIfDoggoWasGood());
 
+// Write a function that accepts the top three bowling scores and returns the average.
+
+let first = 300;
+let second = 185;
+let third = 256;
+
+function avgScore(score1, score2, score3) {
+    let average = (score1 + score2 + score3) / 3;
+    return average;
+}
+console.log("The average score is " + avgScore(first, second, third));
+
+
+// Write a function that accepts the best score and returns true if its divisible by 3 or 5 and false if it’s not
+let highScore = 300;
+
+function bestScore() {
+    if(highScore % 3 ==0 && highScore % 5 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(bestScore());
+
+// Write a function that accepts the name of the bowling place (“ThEbEsTbOwLiNgPlAcEever”) and returns it in all lower case
+// BONUS:write a function that returns “TheBestBowlingPlaceEver” and return it all lowercase with a space between each word
+
+let bowlingPlace = "ThEbEsTbOwLiNgPlAcEever";
+
+function makeLowerCase() {
+    return bowlingPlace.toLowerCase();
+}
+console.log(makeLowerCase(bowlingPlace));
+
+
+// Write a function that returns the type of each of the variables (call the function once per variable)
+let score = 109;
+let name = "bowling Is Fun";
+let isBowlingFun = true;
+let players = ["tom", "jerry", "Garfield"];
+
+function tellType(thing) {
+    return typeof thing;
+}
+
+console.log(tellType(score));
+console.log(tellType(name));
+console.log(tellType(isBowlingFun));
+console.log(tellType(players));
+
+
+// Write a function that executes all of the above functions
+
+function executeAll() {
+    avgScore();
+    bestScore();
+    makeLowerCase();
+    tellType();
+}
+console.log(executeAll());
+
+// Execute the following functions, and find the Error: (Hint: try console logging the result)
+let firstName = "Jason";
+let lastName = "Comstock";
+
+function myFunction() {
+    return (firstName + " " + lastName);
+}
+console.log(myFunction());
+
+let greatness = "Laura";
+function thatsNotMyName(name) {
+    if (name = "Laura") {
+        return "that's me!, I'm " + name;
+    } else {
+        return "that's not my name";
+    }
+}
+
+console.log(thatsNotMyName(greatness));
 
