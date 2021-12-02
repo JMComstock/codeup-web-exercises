7a7826c5a2f5ae5e18b6d7f987463799af93cb1f
JMComstock <jasoncomstock11@gmail.com>
2021-12-01 21:34:11 -0600

    changed function orientation

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,15 +1,15 @@
 function showMultiplicationTable(input) {
-    for(let i = 1; i<8; i++) {
+    for(let i = 1; i < 11; i++) {
         console.log(input * i);
     }
 }
-console.log(showMultiplicationTable(7));
+showMultiplicationTable(7);
 
 
-var random = Math.floor((Math.random() * 200) + 20);
-
-function generateRandom() {
-    for(let i=20; i <=200; i++) {
-        console.log()
-    }
-}
+// var random = Math.floor((Math.random() * 200) + 20);
+//
+// function generateRandom() {
+//     for(let i=20; i <=200; i++) {
+//         console.log()
+//     }
+// }
