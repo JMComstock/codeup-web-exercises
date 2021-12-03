db454b94d9e84344b2b221f8ac281ea44df78f48
JMComstock <jasoncomstock11@gmail.com>
2021-12-03 10:01:32 -0600

    finished exercise

--- js/while.js
+++ js/while.js
@@ -1,26 +1,29 @@
 "use strict";
 
+/*
 let x = 2;
 
-while(x < 65536) {
+while(x <= 65536) {
     console.log(x);
-    x += Math.pow(2, 2);
+    x *= 2;
 }
+*/
 
 
-let allCones = Math.floor(Math.random() * 50) + 50;
-console.log("There is " + allCones + " total cones.")
-let conesLeft = "";
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
 do {
-    let cones = Math.floor(Math.random() * 5) + 1;
-    console.log("The client wants " + cones + " cones");
-    if(allCones < cones) {
-        console.log("Cannot sell " + cones + " cones. I only have " + allCones);
+    var conesToBuy = Math.floor(Math.random() * 5) + 1;
+
+    if (conesToBuy <= allCones) {
+        console.log(conesToBuy + " cones sold...");
+        allCones -= conesToBuy;
     } else {
-        conesLeft = allCones - cones;
-        console.log(cones + " cone(s) sold...");
-        continue;
+        console.log("Cannot sell you " + conesToBuy + " cones I only have " + allCones + "...");
     }
-} while (allCones === 0) {
-    console.log("Yay! You sold them all!");
-}
+    console.log(allCones)
+} while (allCones > 0)
+
+console.log("Yay! I sold all the cones!");
+
