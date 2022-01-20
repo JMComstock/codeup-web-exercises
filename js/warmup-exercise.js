855c0763ac1acbf9747d0f4ac796d730635a9239
JMComstock <jasoncomstock11@gmail.com>
2022-01-20 09:18:41 -0600

    added function to test string for upper/lowerCase and if it is even a letter

--- js/warmup-exercise.js
+++ js/warmup-exercise.js
@@ -1,9 +1,32 @@
-'use script'
+'use strict';
 
 // TODO: Make a program that filters through this array: ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"]
 //  and returns a new array that contains only those whose name have only 4 letters in them.
+//
+// let arrayToFilter = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+//
+// let result = arrayToFilter.filter(word => word.length === 4);
+// console.log(result);
 
-let arrayToFilter = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+// TODO: write a function that accepts a string and checks whether each character in the string is upperCase or lowerCase and returns "letter [insert letter] is uppercase/lowercase", accordingly.
+//  Bonus: return "is not a character" if it's not a letter
+//
+// TODO: input example: "bJI*ENsd4fRAIR5VIrsRFA-ijgRar"
 
-let result = arrayToFilter.filter(word => word.length === 4);
-console.log(result);
+let stringToFiler = "bJI*ENsd4fRAIR5VIrsRFA-ijgRar";
+let i=0;
+let character='';
+while (i <= stringToFiler.length - 1){
+    character = stringToFiler.charAt(i);
+    if (!character.match(/[a-z]/i)){
+        console.log(character + " is not a character");
+    }else{
+        if (character === character.toUpperCase()) {
+           console.log("letter " + character + " is uppercase");
+        }
+        if (character === character.toLowerCase()){
+            console.log("letter " + character + " is lowercase");
+        }
+    }
+    i++;
+}
