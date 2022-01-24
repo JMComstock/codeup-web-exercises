a9de454a6df0d3d9d15dab3178cf29e3c55c2a78
JMComstock <jasoncomstock11@gmail.com>
2022-01-24 15:59:35 -0600

    finished jQuery traversing lecture, jqueryFAQ exercise, and 1/24 warmup exercise

--- js/warmup-exercise.js
+++ js/warmup-exercise.js
@@ -13,20 +13,55 @@
 //
 // TODO: input example: "bJI*ENsd4fRAIR5VIrsRFA-ijgRar"
 
-let stringToFiler = "bJI*ENsd4fRAIR5VIrsRFA-ijgRar";
-let i=0;
-let character='';
-while (i <= stringToFiler.length - 1){
-    character = stringToFiler.charAt(i);
-    if (!character.match(/[A-Za-z]/i)){
-        console.log(character + " is not a character");
-    } else {
-        if (character === character.toUpperCase()) {
-           console.log("letter " + character + " is uppercase");
-        }
-        if (character === character.toLowerCase()){
-            console.log("letter " + character + " is lowercase");
-        }
-    }
-    i++;
-}
+// let stringToFiler = "bJI*ENsd4fRAIR5VIrsRFA-ijgRar";
+// let i=0;
+// let character='';
+// while (i <= stringToFiler.length - 1){
+//     character = stringToFiler.charAt(i);
+//     if (!character.match(/[A-Za-z]/i)){
+//         console.log(character + " is not a character");
+//     } else {
+//         if (character === character.toUpperCase()) {
+//            console.log("letter " + character + " is uppercase");
+//         }
+//         if (character === character.toLowerCase()){
+//             console.log("letter " + character + " is lowercase");
+//         }
+//     }
+//     i++;
+// }
+
+// TODO: PROBLEM:  write a function that accepts a string of numbers and returns the sum of those numbers
+// INPUT 1: "42958372" --- EXPECTED OUTPUT 1: 40
+// INPUT 2: "917485"      --- EXPECTED OUTPUT 2: 34
+
+let input1 = "42958372";
+let input2 = "917485";
+let arr1 = input1.split('');
+let arr2 = input2.split('');
+let numArr1 = [];
+let numArr2 = [];
+arr1.forEach(num => {
+    num = Number(num);
+    numArr1.push(num);
+})
+console.log(numArr1);
+
+arr2.forEach(num => {
+    num = Number(num);
+    numArr2.push(num);
+})
+console.log(numArr2);
+
+// Using reduce function to find the sum
+let sum = numArr1.reduce(function (x,y) {
+    return x + y;
+}, 0);  // return a single value
+
+let sum2 = numArr2.reduce(function (x,y) {
+    return x + y;
+}, 0)
+//
+// document.write("Sum using Reduce method: " + sum);
+console.log("The sum of input1 is: " + sum);
+console.log("The sum of input2 is: " + sum2);
