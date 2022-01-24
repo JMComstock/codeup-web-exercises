5278c44c6c82dcd049e25a1a6f645db694ee08dc
JMComstock <jasoncomstock11@gmail.com>
2022-01-24 10:23:09 -0600

    finished jquery-essential-methods-exercise

--- js/jquery-essential-methods-exercise.js
+++ js/jquery-essential-methods-exercise.js
@@ -11,4 +11,35 @@ $(".border").css("border", "2px solid black");
 // chain a method onto that element
 // pass in the value that I want to change it to
 
-$("h1").text("Happy Monday")
+// SETTERS
+// .html() getting? or setting?  SETTING!
+console.log($("h1").html()); // returns Hello World // getting
+$("h1").html("Happy Monday"); // setting
+
+// GETTERS
+// .html() getting or setting?  // GETTING
+console.log($("h1").html());  // returns Happy Monday // getting
+
+
+// TODO: add a class of border to my h2
+
+$("#red").addClass("border"); // setter
+console.log($("#red"));
+$("#red").removeClass("border"); // setter
+console.log($("#red"))
+
+// .toggleClass()
+
+// toggle our h1 to be highlighted on an event
+//TODO: toggle the h1 to be highlighted on event
+
+// grab the element we want to change
+// grab the element that's listening for the click
+// change the css (.css())
+// toggle it
+
+$("h1").click(function () {
+    $(this).toggleClass("highlight");
+})
+
+
