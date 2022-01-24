7c603db7ea1effdc257451fb325127a66adbcae6
JMComstock <jasoncomstock11@gmail.com>
2022-01-24 09:38:33 -0600

    added more functionality to konami exercise

---
+++ js/jquery-essential-methods-exercise.js
@@ -0,0 +1,14 @@
+// TODO: give all paragraphs a border
+
+// grab all the p elements
+// change the css
+
+$(".border").css("border", "2px solid black");
+
+//TODO: change h1 to say happy monday
+
+// grab the element
+// chain a method onto that element
+// pass in the value that I want to change it to
+
+$("h1").text("Happy Monday")
