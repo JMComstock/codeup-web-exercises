8091ab08b2db50c749acdd58bba6fc28f6062da7
JMComstock <jasoncomstock11@gmail.com>
2022-02-02 17:07:19 -0600

    condensed weathermap.js

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,3 +1,5 @@
+"use strict";
+
 mapboxgl.accessToken = MAPBOX_WEATHER;
 let map = new mapboxgl.Map({
     container: 'map',
@@ -71,88 +73,28 @@ map.on('load', () => {
                 $(".mid").html("");
                 console.log(data);
 
-                let newSource0 = data.list[0].weather[0].icon;
-                let iconSource0 = `https://openweathermap.org/img/w/${newSource0}.png`;
-
-                let newSource8 = data.list[8].weather[0].icon;
-                let iconSource8 = `https://openweathermap.org/img/w/${newSource8}.png`;
-
-                let newSource16 = data.list[0].weather[0].icon;
-                let iconSource16 = `https://openweathermap.org/img/w/${newSource16}.png`;
-
-                let newSource24 = data.list[0].weather[0].icon;
-                let iconSource24 = `https://openweathermap.org/img/w/${newSource24}.png`;
-
-                let newSource32 = data.list[0].weather[0].icon;
-                let iconSource32 = `https://openweathermap.org/img/w/${newSource32}.png`;
-
                 $(".top").append(
                     `<h2 id="h2-top">Weather App</h2>` +
                     `<h6">Current City: ${data.city.name}</h6>`);
-                $(".mid").append(
-                    `<div class="d-flex card m-1" style="width: 15rem;">` +
-                    `<div class="col justify-content-center card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[0].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[0].main.temp_min} \u00B0 F / ${data.list[0].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource0}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[0].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[0].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[0].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[0].main.pressure}</p>` +
-                    `</div>` +
-                    `</div>`);
+                for (let i=0; i< data.list.length; i+=8) {
 
-                $(".mid").append(
-                    `<div class="card m-1" style="width: 15rem;">` +
-                    `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[8].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[8].main.temp_min} \u00B0 F / ${data.list[8].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource8}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[8].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[8].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[8].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[8].main.pressure}</p>` +
-                    `</div>` +
-                    `</div>`);
+                    let dateTime = data.list[i].dt;
+                    let newDate = new Date(dateTime * 1000).toLocaleDateString("en",{weekday: "long"});
+                    let weatherImg = data.list[i].weather[0].icon;
 
-                $(".mid").append(
-                    `<div class="card m-1" style="width: 15rem;">` +
-                    `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[16].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[16].main.temp_min} \u00B0 F / ${data.list[16].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource16}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[16].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[16].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[16].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[16].main.pressure}</p>` +
-                    `</div>` +
-                    `</div>`);
-
-                $(".mid").append(
-                    `<div class="card m-1" style="width: 15rem;">` +
-                    `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[24].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[24].main.temp_min} \u00B0 F / ${data.list[24].main.temp_max} \u00B0 F</h6>` +
-                    `<img class="justify-content-center" src="${iconSource24}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[24].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[24].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[24].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[24].main.pressure}</p>` +
-                    `</div>` +
-                    `</div>`);
-
-                $(".mid").append(
-                    `<div class="card m-1" style="width: 15rem;">` +
-                    `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[32].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[32].main.temp_min} \u00B0 F / ${data.list[32].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource32}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[32].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[32].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[32].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[32].main.pressure}</p>` +
-                    `</div>` +
-                    `</div>`);
+                    $(".mid").append(
+                        `<div class="d-flex card m-1" style="width: 15rem;">` +
+                        `<div class="col justify-content-center card-body">` +
+                        `<h5 class="card-title bg-secondary">${newDate}</h5>` +
+                        `<h6 class="card-subtitle mb-2 text-muted">${data.list[i].main.temp_min} \u00B0 F / ${data.list[0].main.temp_max} \u00B0 F</h6>` +
+                        "<img src='https://openweathermap.org/img/w/" + weatherImg + ".png' alt='weather image'>" +
+                        `<p class="card-text">Desctiption: ${data.list[i].weather[0].description}</p>` +
+                        `<p class="card-text">Humidity: ${data.list[i].main.humidity}</p>` +
+                        `<p class="card-text">Wind: ${data.list[i].wind.speed}</p>` +
+                        `<p class="card-text">Pressure: ${data.list[i].main.pressure}</p>` +
+                        `</div>` +
+                        `</div>`);
+                }
             })
         }
 
@@ -183,93 +125,28 @@ function onDragEnd() {
         $(".mid").html("");
         console.log(data);
 
-        let newSource0 = data.list[0].weather[0].icon;
-        let iconSource0 = `https://openweathermap.org/img/w/${newSource0}.png`;
-
-        let newSource8 = data.list[8].weather[0].icon;
-        let iconSource8 = `https://openweathermap.org/img/w/${newSource8}.png`;
-
-        let newSource16 = data.list[0].weather[0].icon;
-        let iconSource16 = `https://openweathermap.org/img/w/${newSource16}.png`;
-
-        let newSource24 = data.list[0].weather[0].icon;
-        let iconSource24 = `https://openweathermap.org/img/w/${newSource24}.png`;
-
-        let newSource32 = data.list[0].weather[0].icon;
-        let iconSource32 = `https://openweathermap.org/img/w/${newSource32}.png`;
-
-        let dateShorten = [];
-        dateShorten.split("", ${data.list[0].dt_txt});
-        console.log
-
         $(".top").append(
             `<h2 id="h2-top">Weather App</h2>` +
             `<h6">Current City: ${data.city.name}</h6>`);
-        $(".mid").append(
-            `<div class="d-flex card m-1" style="width: 15rem;">` +
+        for (let i=0; i< data.list.length; i+=8) {
+
+            let dateTime = data.list[i].dt;
+            let newDate = new Date(dateTime * 1000).toLocaleDateString("en",{weekday: "long"});
+            let weatherImg = data.list[i].weather[0].icon;
+
+            $(".mid").append(
+                `<div class="d-flex card m-1" style="width: 15rem;">` +
                 `<div class="col justify-content-center card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[0].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[0].main.temp_min} \u00B0 F / ${data.list[0].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource0}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[0].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[0].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[0].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[0].main.pressure}</p>` +
+                `<h5 class="card-title bg-secondary">${newDate}</h5>` +
+                `<h6 class="card-subtitle mb-2 text-muted">${data.list[i].main.temp_min} \u00B0 F / ${data.list[0].main.temp_max} \u00B0 F</h6>` +
+                "<img src='https://openweathermap.org/img/w/" + weatherImg + ".png' alt='weather image'>" +
+                `<p class="card-text">Desctiption: ${data.list[i].weather[0].description}</p>` +
+                `<p class="card-text">Humidity: ${data.list[i].main.humidity}</p>` +
+                `<p class="card-text">Wind: ${data.list[i].wind.speed}</p>` +
+                `<p class="card-text">Pressure: ${data.list[i].main.pressure}</p>` +
                 `</div>` +
-            `</div>`);
-
-
-        $(".mid").append(
-            `<div class="card m-1" style="width: 15rem;">` +
-                `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[8].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[8].main.temp_min} \u00B0 F / ${data.list[8].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource8}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[8].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[8].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[8].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[8].main.pressure}</p>` +
-                `</div>` +
-            `</div>`);
-
-        $(".mid").append(
-            `<div class="card m-1" style="width: 15rem;">` +
-                `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[16].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[16].main.temp_min} \u00B0 F / ${data.list[16].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource16}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[16].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[16].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[16].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[16].main.pressure}</p>` +
-                `</div>` +
-            `</div>`);
-
-        $(".mid").append(
-            `<div class="card m-1" style="width: 15rem;">` +
-                `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[24].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[24].main.temp_min} \u00B0 F / ${data.list[24].main.temp_max} \u00B0 F</h6>` +
-                    `<img class="justify-content-center" src="${iconSource24}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[24].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[24].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[24].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[24].main.pressure}</p>` +
-                `</div>` +
-            `</div>`);
-
-        $(".mid").append(
-            `<div class="card m-1" style="width: 15rem;">` +
-                `<div class="card-body">` +
-                    `<h5 class="card-title bg-secondary">${data.list[32].dt_txt}</h5>` +
-                    `<h6 class="card-subtitle mb-2 text-muted">${data.list[32].main.temp_min} \u00B0 F / ${data.list[32].main.temp_max} \u00B0 F</h6>` +
-                    `<img src="${iconSource32}" alt="">` +
-                    `<p class="card-text">Desctiption: ${data.list[32].weather[0].description}</p>` +
-                    `<p class="card-text">Humidity: ${data.list[32].main.humidity}</p>` +
-                    `<p class="card-text">Wind: ${data.list[32].wind.speed}</p>` +
-                    `<p class="card-text">Pressure: ${data.list[32].main.pressure}</p>` +
-                `</div>` +
-            `</div>`);
+                `</div>`);
+        }
     })
 }
 marker.on('dragend', onDragEnd);
